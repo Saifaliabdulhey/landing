2093a6f5b13da9e5bbf263baa7da437ff48ce62f
On My Own <jspz887@gmail.com>
2022-07-13 03:21:27 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.scss';
+import LandingPage from './components/LandingPage'
+import { BrowserRouter as Router, Routes, Route, Link, Switch, } from 'react-router-dom';
+import AmericanSaoa from './components/states/AmericanSaoa';
+import Header from './components/Header';
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+      <Header />
+        <Routes>
+          <Route path="/landing" element={<LandingPage />} />
+          <Route path="/landing/:id" element={<AmericanSaoa />} />
+        </Routes>
+      </Router>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
